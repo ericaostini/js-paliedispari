9ffe0b7e39cdc0b7519bf01655d516cffc3cd25f
ericaostini <ericaostini14@gmail.com>
2023-10-17 15:26:42 +0200

    fuction reverseWord with split/reverse/join

--- js/script.js
+++ js/script.js
@@ -4,5 +4,14 @@ const alertEl = document.querySelector(".alert");
 
 btn.addEventListener("click", function(){
     let originalWord = word.value.toLowerCase();
-    console.log(originalWord);
+    let reverseW = reverseWord(originalWord);
+    console.log(reverseW);
+
 })
+
+function reverseWord(originalWord){
+    let splitWord = originalWord.split("");
+    let reverseA = splitWord.reverse();
+    let joinA = reverseA.join("");
+    return joinA;
+}
