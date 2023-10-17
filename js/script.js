495262cd5d6ca9e7bfa8be25b2d3c8f1440739c7
ericaostini <ericaostini14@gmail.com>
2023-10-17 15:20:08 +0200

    display the entered word in the console

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 const btn = document.querySelector("button");
 const word = document.getElementById("word");
 const alertEl = document.querySelector(".alert");
+
+btn.addEventListener("click", function(){
+    let originalWord = word.value.toLowerCase();
+    console.log(originalWord);
+})
